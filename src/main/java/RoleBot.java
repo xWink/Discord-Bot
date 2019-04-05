4126dd84908922813848439122d093bf1478bb26
xWink <shawn.s.kaplan@gmail.com>
2019-04-05 04:58:09 -0400

    Testing memory storage

--- src/main/java/RoleBot.java
+++ src/main/java/RoleBot.java
@@ -3,7 +3,9 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 
 public class RoleBot {
+
 	public static void main(String[] args) throws Exception{
+
 		try {
 			// Create bot with token given by Discord developer page
 			JDA api = new JDABuilder(AccountType.BOT).setToken("CHANGETOKEN").build();

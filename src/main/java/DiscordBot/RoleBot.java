93fd11e73fba0050f49f085b904d4217cae6fb1d
xWink <shawn.s.kaplan@gmail.com>
2019-04-06 17:33:53 -0400

    RoleBot now gets token from token.txt

--- src/main/java/DiscordBot/RoleBot.java
+++ src/main/java/DiscordBot/RoleBot.java
@@ -3,13 +3,17 @@ package DiscordBot;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import java.util.Scanner;
 
 public class RoleBot {
 	public static void main(String[] args) throws Exception{
 
 		try {
+			Scanner scanner = new Scanner("token.txt");
+			String token = scanner.nextLine();
+			scanner.close();
 			// Create bot with token given by Discord developer page
-			JDA api = new JDABuilder(AccountType.BOT).setToken("CHANGETOKEN").build();
+			JDA api = new JDABuilder(AccountType.BOT).setToken(token).build();
 			api.addEventListener(new MyEventListener());
 		}catch (Exception e){
 			e.printStackTrace();

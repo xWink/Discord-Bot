f1863bd8748597cbfae956698f3a869196fdf95e
xWink <shawn.s.kaplan@gmail.com>
2019-04-24 02:46:34 -0400

    Turns out there wasn't anything wrong with the code ezclap

--- src/main/java/DiscordBot/commands/Join.java
+++ src/main/java/DiscordBot/commands/Join.java
@@ -22,7 +22,6 @@ public class Join {
 			return;
 		}
 		// If role exists and isn't restricted, assign user to role
-		System.out.println(guild.getRolesByName(roleName,true).get(0).getName());
 		if (guild.getRolesByName(roleName,true).get(0).getName().toLowerCase().equals(roleName.toLowerCase())) {
 			guild.getController().addRolesToMember(auth, guild.getRolesByName(roleName, true)).queue();
 			channel.sendMessage("Role \""+roleName+"\" added to "+auth.getAsMention()).queue();

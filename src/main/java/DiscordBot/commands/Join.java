5eaa5533eb8315a0b3d969d88e9d6f67ede08032
xWink <shawn.s.kaplan@gmail.com>
2019-04-24 03:06:15 -0400

    Found another issue with !join. Checking logic flow

--- src/main/java/DiscordBot/commands/Join.java
+++ src/main/java/DiscordBot/commands/Join.java
@@ -21,8 +21,10 @@ public class Join {
 			channel.sendMessage("I cannot set you to that role").queue();
 			return;
 		}
+		System.out.println("Here");
 		// If role exists and isn't restricted, assign user to role
 		if (guild.getRolesByName(roleName,true).get(0).getName().toLowerCase().equals(roleName.toLowerCase())) {
+			System.out.println("Here0");
 			guild.getController().addRolesToMember(auth, guild.getRolesByName(roleName, true)).queue();
 			channel.sendMessage("Role \""+roleName+"\" added to "+auth.getAsMention()).queue();
 		}

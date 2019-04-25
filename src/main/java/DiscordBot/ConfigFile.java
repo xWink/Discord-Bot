0c7650b0b9c242c1f8676e17502333e06da9dc76
rafaelwi <rafwiskailnicki@gmail.com>
2019-04-25 12:50:38 -0400

    Made channel var an ArrayList

--- src/main/java/DiscordBot/ConfigFile.java
+++ src/main/java/DiscordBot/ConfigFile.java
@@ -1,9 +1,11 @@
 // Config File Object
 package DiscordBot;
 
+import java.util.ArrayList;
+
 class ConfigFile {
     public String token;
-    public String channel;
+    public ArrayList<String> channel;
     public String applicant_path;
     public String score_path;
     public String roulette_path;

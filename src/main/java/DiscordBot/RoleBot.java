2d1d6c3c24d06c0000d221d6c61409f0786e8d11
rafaelwi <rafwiskailnicki@gmail.com>
2019-04-24 22:11:01 -0400

    Replaced old file reading with GetConfig

--- src/main/java/DiscordBot/RoleBot.java
+++ src/main/java/DiscordBot/RoleBot.java
@@ -6,15 +6,20 @@ import net.dv8tion.jda.core.JDABuilder;
 
 import java.io.File;
 import java.util.Scanner;
+import DiscordBot.GetConfig;
 
 public class RoleBot {
 	public static void main(String[] args) throws Exception{
 
 		try {
+			/*
 			File file = new File("token.txt");
 			Scanner scanner = new Scanner(file);
 			String token = scanner.nextLine();
 			scanner.close();
+			*/
+			String token = GetConfig.getConfig();
+
 
 			// Create bot with token given by Discord developer page
 			JDA api = new JDABuilder(AccountType.BOT).setToken(token).build();

f1bd745a902b314968c8774ce79a4b3710822048
rafaelwi <rafwiskailnicki@gmail.com>
2019-04-24 22:44:30 -0400

    Created ConfigFile obj

---
+++ src/main/java/DiscordBot/ConfigFile.java
@@ -0,0 +1,10 @@
+// Config File Object
+package DiscordBot;
+
+class ConfigFile {
+    public String token;
+    public String channel;
+    public String applicant_path;
+    public String score_path;
+    public String roulette_path;
+}

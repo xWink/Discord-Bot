83525425f358565507f7c7a493816e75ac2c8a22
xWink <shawn.s.kaplan@gmail.com>
2019-05-10 02:09:13 -0400

    Made bangscores output more consistent

--- src/main/java/DiscordBot/commands/BangScores.java
+++ src/main/java/DiscordBot/commands/BangScores.java
@@ -12,10 +12,10 @@ public class BangScores {
         BangHighScores highScores = GetBangScores.getBangScores(guild);
 
         if (highScores != null){
-            channel.sendMessage("The player with the most attempts is "+highScores.mostAttemptsPlayer+" with a score of "+(int)highScores.attemptCount).queue();
-            channel.sendMessage("The player with the most deaths is "+highScores.mostDeathsPlayer+" with a score of "+(int)highScores.deathCount).queue();
-            channel.sendMessage("The player with the best survival rate is "+highScores.luckiest+" at "+highScores.bestRate+"%").queue();
-            channel.sendMessage("The player with the worst survival rate is "+highScores.unluckiest+" at "+highScores.worstRate+"%").queue();
+            channel.sendMessage("The player with the most attempts is "+highScores.mostAttemptsPlayer+" with a score of "+(int)highScores.attemptCount).complete();
+            channel.sendMessage("The player with the most deaths is "+highScores.mostDeathsPlayer+" with a score of "+(int)highScores.deathCount).complete();
+            channel.sendMessage("The player with the best survival rate is "+highScores.luckiest+" at "+highScores.bestRate+"%").complete();
+            channel.sendMessage("The player with the worst survival rate is "+highScores.unluckiest+" at "+highScores.worstRate+"%").complete();
         }
     }
 }

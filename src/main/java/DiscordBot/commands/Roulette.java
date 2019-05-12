c52cf4577f7fa300b0a4786bc32d7094d671f780
xWink <shawn.s.kaplan@gmail.com>
2019-05-11 23:39:42 -0400

    Trying to fix bug where survival/death is not displayed but chamber count is

--- src/main/java/DiscordBot/commands/Roulette.java
+++ src/main/java/DiscordBot/commands/Roulette.java
@@ -44,27 +44,27 @@ public class Roulette {
 					boom = 0;
 					chamberCount = 6;
 					jammed = 1;
-					channel.sendMessage("The gun jammed... " + author.getName() + " survived <:poggers:564285288621539328> <:poggers:564285288621539328> <:poggers:564285288621539328>").queue();
+					channel.sendMessage("The gun jammed... " + author.getName() + " survived <:poggers:564285288621539328> <:poggers:564285288621539328> <:poggers:564285288621539328>").complete();
 				}
 				// If the gun doesn't jam with one chamber left, boom
 				else {
 					boom = 1;
 					chamberCount = 6;
-					channel.sendMessage("Bang! " + author.getName() + " died :skull:").queue();
+					channel.sendMessage("Bang! " + author.getName() + " died :skull:").complete();
 				}
 			}
 			// If there is more than 1 chamber left, boom
 			else {
 				boom = 1;
 				chamberCount = 6;
-				channel.sendMessage("Bang! " + author.getName() + " died :skull:").queue();
+				channel.sendMessage("Bang! " + author.getName() + " died :skull:").complete();
 			} 
 		}
 		// No boom
 		else {
 			boom = 0;
 			chamberCount--;
-			channel.sendMessage("Click. " + author.getName() + " survived  <:poggies:564285288621539328>").queue();
+			channel.sendMessage("Click. " + author.getName() + " survived  <:poggies:564285288621539328>").complete();
 		}
 
 		channel.sendMessage("Chambers left in the cylinder: ||  "+chamberCount+"  ||").queue();

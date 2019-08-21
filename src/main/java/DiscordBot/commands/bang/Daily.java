6c08f32e4bf7b9260c881f23040b1d6398db95e9
Shawn <shawn.s.kaplan@gmail.com>
2019-08-21 18:33:26 -0400

    Added command to see daily reset time

---
+++ src/main/java/DiscordBot/commands/bang/Daily.java
@@ -0,0 +1,40 @@
+package DiscordBot.commands.bang;
+
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Daily {
+
+    // Acquire user's daily time from database
+    private static long getDaily(User author, Connection conn, MessageChannel channel){
+
+        try{
+            PreparedStatement st = conn.prepareStatement("SELECT last_daily FROM bang WHERE user="+author.getIdLong());
+            ResultSet rs = st.executeQuery();
+            rs.next();
+
+            // Add 24 hours to the time
+            return rs.getLong("last_daily") + 86400000;
+        }
+        catch (SQLException e){
+            channel.sendMessage("Error fetching your daily time, please contact a moderator!").queue();
+        }
+
+        return -1;
+    }
+
+    // Output that time as a date/time
+    public static void daily(User author, Connection conn, MessageChannel channel) {
+
+        // Format acquired date to desired style
+        SimpleDateFormat df = new SimpleDateFormat("MMM dd hh:mm:ss");
+        Date date = new Date(getDaily(author, conn, channel));
+
+        channel.sendMessage("Next daily available at: `"+df.format(date)+"`").queue();
+    }
+}

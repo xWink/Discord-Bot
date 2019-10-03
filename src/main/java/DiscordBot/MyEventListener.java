d624e7faaf87fec7c70227c97e7700d144cc87d5
Shawn <shawn.s.kaplan@gmail.com>
2019-10-03 14:58:53 -0400

    Testing message reaction events 5

--- src/main/java/DiscordBot/MyEventListener.java
+++ src/main/java/DiscordBot/MyEventListener.java
@@ -65,12 +65,12 @@ public class MyEventListener extends ListenerAdapter {
 
 	@Override
 	public void onMessageReactionAdd(MessageReactionAddEvent event) {
-		System.out.println(event.getReactionEmote().getEmote().getIdLong());
+		System.out.println(event.getReactionEmote().getEmote().getName());
 		long messageAuthId = event.getTextChannel().getMessageById(event.getMessageId())
 				.complete().getAuthor().getIdLong();
 
 
-		if (event.getReactionEmote().getEmote().getIdLong() != 0) {
+		if (event.getReactionEmote().getEmote().getName().equals("arrow_up")) {
 			if (this.conn != null) {
 				System.out.println("SUCCESS");
 				/*try {

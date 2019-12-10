5ab6bb637150ed24c1bb7e0e9399193c1965904d
Shawn <shawn.s.kaplan@gmail.com>
2019-12-10 02:50:59 -0500

    Added blackjack to command list

--- src/command/CommandList.java
+++ src/command/CommandList.java
@@ -1,5 +1,9 @@
 package command;
 
+import command.commands.blackjack.Bet;
+import command.commands.blackjack.Hit;
+import command.commands.blackjack.MyHand;
+import command.commands.blackjack.Stand;
 import command.commands.economy.Buy;
 import command.commands.bang.Daily;
 import command.commands.Help;
@@ -12,6 +16,7 @@ import command.commands.admin.Purge;
 import command.commands.economy.Wallet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Container class for a list of all the commands
@@ -55,5 +60,7 @@ public final class CommandList {
         commands.add(new Info());
         commands.add(new Market());
         commands.add(new Buy());
+        // Blackjack
+        commands.addAll(Arrays.asList(new Bet(), new Hit(), new MyHand(), new Stand()));
     }
 }

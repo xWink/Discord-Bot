a1039a65eaa2386224bdd1c2cbb458ee93fc5b7f
Shawn Kaplan <shawn.s.kaplan@gmail.com>
2019-12-29 04:58:23 -0500

    Added package-info.java

---
+++ src/command/util/highscores/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes in this package define the high scores counted
+ * from games. These high scores can be updated and converted
+ * to strings representing the results of the high scores.
+ */
+
+package command.util.highscores;

19bcbcebf8c8ce9ad5e30b55f9a888bf1c724ddc
Shawn Kaplan <shawn.s.kaplan@gmail.com>
2019-12-30 14:48:14 -0500

    Added package-info.java

---
+++ src/command/util/game/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes in this package define the games available in this bot.
+ * Games are stored in memory in an associated List class (ie. BlackJackList.java).
+ * Games are played by players, which are defined in the Player class.
+ */
+
+package command.util.game;

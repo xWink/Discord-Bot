18057fe5019db477edfedf78d2f8b8e7a31cb34d
Nicholas Rosati <nicholas@rosati.xyz>
2020-02-07 15:49:00 -0500

    Add logging functionality
    Relies on `log4j.properties` in the classpath

--- src/main/RoleBot.java
+++ src/main/RoleBot.java
@@ -8,6 +8,7 @@ import main.timertasks.DiscussionPurge;
 import main.timertasks.PruneBangStreaks;
 import main.timertasks.RemoveExpiredRoles;
 import main.timertasks.UpdateHighScores;
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -30,6 +31,7 @@ public class RoleBot {
      * @param args ignored
      */
     public static void main(String[] args) {
+        BasicConfigurator.configure();
         try {
             logger.debug("Entering application.");
             Server.getApi().addEventListener(

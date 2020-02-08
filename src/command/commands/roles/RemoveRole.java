1a5d90960feb3c25e39cf3eaf633a33e6f25ec5a
Shawn Kaplan <shawn.s.kaplan@gmail.com>
2020-02-08 02:19:50 -0500

    Added RemoveRole command

---
+++ src/command/commands/roles/RemoveRole.java
@@ -0,0 +1,47 @@
+package command.commands.roles;
+
+import command.ResponseCommand;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class RemoveRole extends ResponseCommand {
+
+    private String roleName;
+
+    /**
+     * Initializes the command's key to "!removerole".
+     */
+    public RemoveRole() {
+        super("!removerole", false);
+    }
+
+    @Override
+    public boolean keyMatches(String string) {
+        return string.toLowerCase().startsWith(getKey()) && string.split(" ").length == 2;
+    }
+
+    @Override
+    public void respond(MessageReceivedEvent event) {
+        if (event.getMessage().getContentRaw().toLowerCase().equals("yes")) {
+            Role role = event.getGuild().getRolesByName(roleName, true).get(0);
+            if (role == null) {
+                event.getChannel().sendMessage("Could not find such a role").queue();
+            } else if (!(role.getName().equals("Blue") || role.getName().equals("Pink") || role.getName().equals("Green")
+                    || role.getName().equals("Orange") || role.getName().equals("Purple"))) {
+                event.getChannel().sendMessage("I cannot remove this role").queue();
+            } else {
+                event.getGuild().removeRoleFromMember(event.getAuthor().getId(), role).queue();
+                event.getChannel().sendMessage("You no longer have this role").queue();
+            }
+        }
+    }
+
+    @Override
+    public void start(MessageReceivedEvent event) {
+        if (event.getMember() == null)
+            return;
+
+        roleName = event.getMessage().getContentRaw().split(" ")[1];
+        new ResponseHandler(event.getChannel().getIdLong(), event.getAuthor().getIdLong());
+    }
+}

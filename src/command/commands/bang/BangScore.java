16669d100c5db051fc56bccdfd22e50d0a33559e
christopherwalkerml <christopherwalkerml@gmail.com>
2020-02-09 15:20:05 -0500

    Fixed conflicts in BangScore(s)

--- src/command/commands/bang/BangScore.java
+++ src/command/commands/bang/BangScore.java
@@ -3,8 +3,7 @@ package command.commands.bang;
 import command.Command;
 import command.util.highscores.BangHighScore;
 import main.Server;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class BangScore extends Command {
 
@@ -16,23 +15,13 @@ public class BangScore extends Command {
     }
 
     /**
-     * Matches with either "!bangscore" or "!bangscores"
-     * @param string the user's input being compared to the key
-     * @return true if key matches "!bangscore" or "!bangscores"
-     */
-    @Override
-    public boolean keyMatches(String string) {
-        return string.toLowerCase().matches("^" + getKey() + "s?$");
-    }
-
-    /**
      * Prints the high scores for bang.
      *
      * @param event the MessageReceivedEvent that triggered the command
      */
     @Override
     public void start(MessageReceivedEvent event) {
-        if (event.getChannel().getIdLong() != Server.getSpamChannel()
+        if (event.getChannel().getIdLong() != 674369527731060749L
                 && event.getChannel().getIdLong() != Server.getBotsChannel()) {
             return;
         }

40d11685ae7cd73fd9ca1dcc7798434c51aa2a2f
Shawn Kaplan <shawn.s.kaplan@gmail.com>
2020-04-14 02:54:20 -0400

    Added timertask to cleanup the messages table

---
+++ src/main/timertasks/CleanMessageTable.java
@@ -0,0 +1,26 @@
+package main.timertasks;
+
+import database.connectors.MessagesConnector;
+
+import java.util.TimerTask;
+
+public class CleanMessageTable extends TimerTask {
+
+    /**
+     * Backs up all messages older than a day and removes them from
+     * the messages table.
+     *
+     * @see MessagesConnector
+     */
+    @Override
+    public void run() {
+        final int pruneThreshold = 1000 * 60 * 60 * 24;
+        MessagesConnector mc = new MessagesConnector();
+        try {
+            mc.backupMessages(pruneThreshold);
+            mc.pruneMessages(pruneThreshold);
+        } catch (Exception e) {
+            System.out.println("Failed to backup messages");
+        }
+    }
+}

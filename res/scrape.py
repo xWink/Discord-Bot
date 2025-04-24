7356920ec371d07d69dac1156381103447ff57ed
jere-dev <jeremthom1@gmail.com>
2025-04-24 01:37:55 -0400

    update scrape.py

--- res/scrape.py
+++ res/scrape.py
@@ -12,7 +12,7 @@ prereqs = []  # List to store the prerequisites
 descriptions = []  # List to store the descriptions
 links = []  # List to store links to all the links to courses
 
-URL = "https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12/index.shtml"
+URL = "https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/"
 
 # gets HTML of the course directory page
 courseDir = requests.get(URL)
@@ -20,56 +20,72 @@ courseDirHTML = courseDir.content
 
 # setup parser for getting links
 soup = BeautifulSoup(courseDirHTML, "html.parser")
-
-linksHTML = soup.find('div', attrs={'class': 'subnav'})
+linksHTML = soup.find(class_="az_sitemap")
 
 # gets the links of out the html and stores them in links list
 for link in linksHTML.findAll('a', href=True):
-    links.append(link['href'][1:])  # [1:] to get rid of leading period in link
-
-# gets rid of the first link because its just in the index link
-links.pop(0)
+    if link.get('href')[0] != "#":
+        links.append(link.get('href'))
 
 for link in links:
-    URL = "https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12" + link
+    URL = "https://calendar.uoguelph.ca" + link
 
     content = requests.get(URL)
     contentHTML = content.content
 
     soup = BeautifulSoup(contentHTML, "html.parser")
     #  loops through all courses on site
-    for a in soup.findAll('div', attrs={'class': 'course'}):
+    for a in soup.findAll(class_="courseblock"):
 
         #  gets name of course
-        name = a.find('tr', attrs={'class': 'title'})
+        name = ""
+        tmp = a.find(class_="text detail-code margin--small text--semibold text--big")
+        if(tmp):
+            name += tmp.text +" "
+
+        tmp = a.find(class_="text detail-title margin--small text--semibold text--big")
+        if(tmp):
+            name += tmp.text +" "
+            
+        tmp = a.find(class_="text detail-typically_offered margin--small text--semibold text--big")
+        if(tmp):
+            name += tmp.text +" "
+
+        tmp = a.find(class_="text detail-inst_method margin--small text--semibold text--big")
+        if(tmp):
+            name += tmp.text +" "
+
+        tmp = a.find(class_="text detail-hours_html margin--small text--semibold text--big")
+        if(tmp):
+            name += tmp.text + " "
+
 
         #  gets course restrictions
-        if a.find('tr', attrs={'class': 'restrictions'}):
-            restrictions = a.find('tr', attrs={'class': 'restrictions'})
-            restrictions = " ".join(restrictions.text.split())
+        if a.find(class_="text detail-restriction margin--default"):
+            restrictions = a.find(class_="text detail-restriction margin--default").text
+            restrictions = " ".join(restrictions.split())
             restricts.append(restrictions)
         else:
             restricts.append("NONE")
 
-        #  gets course prerequisites
-        if a.find('tr', attrs={'class': 'prereqs'}):
-            prerequisites = a.find('tr', attrs={'class': 'prereqs'})
+         #  gets course prerequisites
+        if a.find(class_="text detail-prerequisite_s_ margin--default"):
+            prerequisites = a.find(class_="text detail-prerequisite_s_ margin--default")
             prerequisites = " ".join(prerequisites.text.split())
             prereqs.append(prerequisites)
         else:
             prereqs.append("NONE")
-
+        
         #  gets course description
-        if a.find('tr', attrs={'class': 'description'}):
-            description = a.find('tr', attrs={'class': 'description'})
+        if a.find(class_="courseblockextra noindent"):
+            description = a.find(class_="courseblockextra noindent")
             description = " ".join(description.text.split())  # super jank way of removing all the extra space
             descriptions.append(description)
         else:
             descriptions.append("NONE")
 
-        titles.append(name.text.strip())
+        titles.append(name.strip())
 
 #  sends all data to a CSV
 df = pd.DataFrame({'Course Title': titles, 'Restrictions': restricts, 'Prerequisites': prereqs, 'Descriptions': descriptions})
 df.to_csv('courses.tsv', index=False, encoding='utf-8-sig', sep="\t")
-
